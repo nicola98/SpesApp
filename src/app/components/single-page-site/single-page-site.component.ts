449e465347f170ec3c5bbe930c9b646d4ab2b9f8
giacomo-mangugan <giacomomangugan09091998@gmail.com>
2018-04-04 10:30:39 +0200

    generate component Single Page Site

---
+++ src/app/components/single-page-site/single-page-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-page-site',
+  templateUrl: './single-page-site.component.html',
+  styleUrls: ['./single-page-site.component.css']
+})
+export class SinglePageSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
