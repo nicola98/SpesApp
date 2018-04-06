f6ed420380c921fa35523838be6d639b0f548b90
vanessalandauro <landauro.kelly@gmail.com>
2018-04-06 12:19:14 +0200

    footer inizio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
