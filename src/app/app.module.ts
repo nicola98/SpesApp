6a89629d34a16af81315dbe8b0f1ff75a032f1a0
vanessalandauro <landauro.kelly@gmail.com>
2018-04-10 12:30:58 +0200

    footer fix variety

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,21 @@ import { FormsModule} from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { FooterComponent } from './components/footer/footer.component';
-=======
 import { SinglePageSiteComponent } from './components/single-page-site/single-page-site.component';
 import { HeaderComponent } from './components/header/header.component';
->>>>>>> origin/Single-Page-Site
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
+    FooterComponent,
     SinglePageSiteComponent,
     HeaderComponent
->>>>>>> origin/Single-Page-Site
   ],
   imports: [
     BrowserModule,
-    FormsModule 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
