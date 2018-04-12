c52f98310ee8a5455187c16a4439efa6267436ea
ilcanna <illnicolacannato@gmail.com>
2018-04-13 00:00:22 +0200

    modifica mock per lista list

--- src/app/mock/listaList.mock.ts
+++ src/app/mock/listaList.mock.ts
@@ -12,12 +12,12 @@ export const LISTALISTMOCK = [
          ] 
      },
      {
-         id: 3, name: "spesa2", image: "../../../assets/elenco-liste/vino.jpg", userId: 1, description:"spesa2", item: [
+         id: 3, name: "spesa3", image: "../../../assets/elenco-liste/vino.jpg", userId: 1, description:"spesa2", item: [
              { id: 2, name: "Pasta", image: "-", dose: "100gr", quantity: "2", gotIt: false }
          ] 
      },
      {
-         id: 4, name: "spesa2", image: "../../../assets/elenco-liste/snack.jpg", userId: 1, description:"spesa2", item: [
+         id: 4, name: "spesa4", image: "../../../assets/elenco-liste/snack.jpg", userId: 1, description:"spesa2", item: [
              { id: 2, name: "Pasta", image: "-", dose: "100gr", quantity: "2", gotIt: false }
          ] 
      }
