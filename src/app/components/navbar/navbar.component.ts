bea5ec9e3e9fcc387ca223d5996ac2541d6cd712
AlePelo <alessandropeloso95@gmail.com>
2018-04-13 12:26:35 +0200

    dropdown home

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +10,13 @@ export class NavbarComponent implements OnInit {
 
   actUser : string = "ciao";
 
-  constructor() {
+  constructor(private router: Router) {
     this.actUser=sessionStorage.getItem("user");
+   }
+
+   logOut(){
+    sessionStorage.setItem("logged", "false");
+    this.router.navigate(["/single-page-site"]);
    }
 
   ngOnInit() {
