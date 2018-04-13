9e357a8831deb736002926a5b0bc150553fe9b5a
ilcanna <illnicolacannato@gmail.com>
2018-04-13 12:11:31 +0200

    aggiunta immagine in dettaglio lista

--- src/app/mock/listItem.mock.ts
+++ src/app/mock/listItem.mock.ts
@@ -1,8 +1,10 @@
 export const LISTITEMTMOCK =
 {
     id: 1, name: "Spesa", image: "../../../assets/elenco-liste/carne.jpg", userId: 1, description:"spesa", item : [
-            {id: 1, name: "Pasta", image: "../../assets/img/product_1.png", dose: "100gr", quantity: "2", gotIt: false },
-            {id: 2, name: "Pasta", image: "../../assets/img/product_2.png", dose: "100gr", quantity: "2", gotIt: false }
+            {id: 1, name: "Pasta", image: "../../assets/img/product_1.png", dose: "100gr", quantity: "2", gotIt: true },
+            {id: 2, name: "Pasta", image: "../../assets/img/product_2.png", dose: "100gr", quantity: "2", gotIt: false },
+            {id: 3, name: "Pasta", image: "../../assets/img/product_1.png", dose: "100gr", quantity: "2", gotIt: false },
+            {id: 4, name: "Pasta", image: "../../assets/img/product_2.png", dose: "100gr", quantity: "2", gotIt: true }
         ]
             
 }
