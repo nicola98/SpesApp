95db6b123cd0376b06f3e79bffe371a63018bdbd
vanessalandauro <landauro.kelly@gmail.com>
2018-04-13 14:56:08 +0200

    aggiunta grafica ricette

--- src/app/mock/listRicette.mock.ts
+++ src/app/mock/listRicette.mock.ts
@@ -1,32 +1,44 @@
 export const LISTRICETTEMOCK = [
   {
-      id: 1, name: "ricetta1", image: "../../../assets/elenco-liste/carne.jpg", userId: 1, description:"spesa", item : [
-            {id: 1, name: "Pasta", image: "../../assets/img/product_1.png", dose: "50gr", quantity: "2", gotIt: true }
+      id: 1, name: "Torta", image: "../../../assets/elenco-liste/cake.jpg", userId: 1, description:"spesa", item : [
+            {id: 1, name: "Farina", image: "../../assets/elenco-liste/farina.jpg", dose: "50gr", gotIt: true },
+            {id: 2, name: "Uova", image: "../../assets/elenco-liste/uova.jpg", dose: "50gr", gotIt: true },
+            {id: 3, name: "Burro", image: "../../assets/elenco-liste/burro.jpg", dose: "50gr", gotIt: true }
           ]
 
   },
    {
-       id: 2, name: "ricetta2", image: "../../../assets/elenco-liste/pesce.jpg", userId: 1, description:"spesa2", item: [
+       id: 2, name: "Pasta al pesto", image: "../../../assets/elenco-liste/pesto.jpg", userId: 1, description:"spesa2", item: [
 
-        {id: 3, name: "Ravioli", image: "../../assets/img/product_3.png", dose: "150gr", quantity: "2", gotIt: true },
-        {id: 4, name: "Trofie", image: "../../assets/img/product_4.png", dose: "30gr", quantity: "2", gotIt: false }
+        {id: 1, name: "Burro", image: "../../assets/elenco-liste/burro.jpg", dose: "150gr", gotIt: true },
+        {id: 2, name: "Basilico", image: "../../assets/elenco-liste/basilico.jpg", dose: "30gr", gotIt: false },
+        {id: 3, name: "Olio", image: "../../assets/elenco-liste/olio.jpg", dose: "150gr", gotIt: true },
+        {id: 4, name: "Pinoli", image: "../../assets/elenco-liste/pinoli.jpg", dose: "150gr", gotIt: true },
+        {id: 5, name: "Pasta", image: "../../assets/elenco-liste/pasta.jpg", dose: "150gr", gotIt: true },
        ]
    },
    {
-       id: 3, name: "ricetta3", image: "../../../assets/elenco-liste/vino.jpg", userId: 1, description:"spesa2", item: [
-        {id: 1, name: "Pasta", image: "../../assets/img/product_1.png", dose: "50gr", quantity: "2", gotIt: true },
-        {id: 2, name: "Pane", image: "../../assets/img/product_2.png", dose: "100gr", quantity: "2", gotIt: false },
+       id: 3, name: "Lasagna", image: "../../../assets/elenco-liste/lasagna.jpg", userId: 1, description:"spesa2", item: [
+        {id: 1, name: "Besciamella", image: "../../assets/elenco-liste/besciamella.jpg", dose: "50gr", gotIt: true },
+        {id: 2, name: "Sale", image: "../../assets/elenco-liste/sale.jpg", dose: "100gr", gotIt: false },
+        {id: 3, name: "Latte intero", image: "../../assets/elenco-liste/latte_intero.jpg", dose: "1L", gotIt: false },
+        {id: 4, name: "Ragu", image: "../../assets/elenco-liste/ragu.jpg", dose: "100gr", gotIt: false }
        ]
    },
    {
-       id: 4, name: "ricetta4", image: "../../../assets/elenco-liste/snack.jpg", userId: 1, description:"spesa2", item: [
-        {id: 4, name: "Trofie", image: "../../assets/img/product_4.png", dose: "30gr", quantity: "2", gotIt: false }
+       id: 4, name: "Caramelle", image: "../../../assets/elenco-liste/caramelle.jpg", userId: 1, description:"spesa2", item: [
+        {id: 1, name: "Zucchero di canna", image: "../../assets/elenco-liste/zucchero.jpg", dose: "30gr", gotIt: false },
+        {id: 2, name: "Limone", image: "../../assets/elenco-liste/limone.jpg", dose: "30gr", gotIt: false },
+        {id: 3, name: "Gelatina alimentare", image: "../../assets/elenco-liste/gelatina.jpg", dose: "30gr", gotIt: false },
+        {id: 4, name: "Colorante alimentare", image: "../../assets/elenco-liste/colore.jpg", dose: "30gr", gotIt: false }
        ]
    },
 
    {
-      id: 5, name: "ricetta5", image: "../../../assets/elenco-liste/snack.jpg", userId: 1, description:"spesa2", item: [
-
+      id: 5, name: "Polpette", image: "../../../assets/elenco-liste/polpette.jpg", userId: 1, description:"spesa2", item: [
+        {id: 1, name: "Ragu", image: "../../assets//elenco-liste/ragu.jpg", dose: "30gr", gotIt: false },
+        {id: 2, name: "Carne", image: "../../assets//elenco-liste/carne.jpg", dose: "30gr", gotIt: false },
+        {id: 3, name: "Sale fino", image: "../../assets//elenco-liste/sale.jpg", dose: "30gr", gotIt: false }
       ]
    }
 ]
